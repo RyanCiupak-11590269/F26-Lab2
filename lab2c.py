4940f74e684d07b03a335457f0c3a40859684bf5
Ryan Ciupak <rciupak@myseneca.ca>
2026-09-16 19:54:41 +0000

    Completed lab 2c

--- lab2c.py
+++ lab2c.py
@@ -2,8 +2,8 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Ryan Ciupak
+# Date: Sept 16, 2026
 # Purpose: Practice using if, elif, and else statments.
 # Usage: ./lab2c.py
 
@@ -17,6 +17,15 @@
 # If they are equal then print:
 # ---- and ---- are equal.
 # Get input from the user
+str1 = (input("Write out a sentence."))
+str2 = (input("How about a second sentence?"))
 
+x = len(str1)
+y = len(str2)
 
-
+if (x > y):
+    print(str1, "is greater than", str2)
+elif (y >x):
+    print(str2, "is greater than", str1)
+elif (x == y):
+    print(str1, "and", str2, "are equal.")
