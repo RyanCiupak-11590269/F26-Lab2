ec4b34d9ea5a2da2f6ba43d22dc49fc1edf4cc8f
Ryan Ciupak <rciupak@myseneca.ca>
2026-09-16 20:02:33 +0000

    Completed lab 2d

--- lab2d.py
+++ lab2d.py
@@ -1,15 +1,19 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date: Learn how to use command line arguments.
-# Purpose: .
+# Author: Ryan Ciupak
+# Date: Sept 16, 2026
+# Purpose:  Learn how to use command line arguments.
 # Usage: ./lab2d.py
 
-
-# TO DO 1: copy the required lines from README.md to print version, platform, argv and the length of argv.
-# run the script in the terminal using command: python ./lab2d.py
-
-# TO DO 2: copy the required lines from README.md to print argv[0], argv[1] and argv[2]
-# run the script using the following command: python lab2d.py maija Maija
-
+import sys
+print(sys.version) # prints the version of the python currently in use.
+print(sys.platform) # prints the name of operating system.
+print(sys.argv) # prints the list of all arguments given at the command line when running our python script from terminal.
+print(len(sys.argv)) # tells us the number of command line arguments the user provides from terminal.
+print(sys.argv[0]) # prints the first argument, it is always the name of script.
+print(sys.argv[1]) # prints the second argument .
+print(sys.argv[2]) # prints the third argument.
+print(len(sys.argv)) # tells us the number of command line arguments the user provides from terminal.
+#The sys.argv gives all arguments given in the command provided in the command line. It displays the name of this file, 
+# maija, and Maija.
