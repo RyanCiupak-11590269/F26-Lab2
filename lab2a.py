3a141a4daa3c9cb8a60d9df4ed0e6cca9de06b04
Ryan Ciupak <rciupak@myseneca.ca>
2026-09-16 19:39:15 +0000

    Completed Lab 2a

--- lab2a.py
+++ lab2a.py
@@ -1,13 +1,17 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Ryan Ciupak
+# Date: Sept 16, 2026
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: ./lab2a.py
 
-# TO DO 1: Follow the instructions given in README.md file
 
-
-
-
+x = input("Choose a number")
+print(type(x))
+x = int(x)
+print(type(x))
+if (x >= 6) is True:
+    print(x,"is greater than 6!")
+if (x > 4) and (x < 12) is True:
+    print(x, "is greater than 4 and less than 12!")
