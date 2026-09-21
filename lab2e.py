6a97d417145b4dff53a816b7ef22abb1fefab692
Ryan Ciupak <rciupak@myseneca.ca>
2026-09-21 20:06:10 +0000

    Completed

--- lab2e.py
+++ lab2e.py
@@ -1,9 +1,17 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Ryan Ciupak
+# Date: Sept 16, 2026
 # Purpose: Learn how to use command line arguments.
 # Usage: ./lab2e.py
 
 # TO DO 1: Follow the instructions given in README.md file
+import sys
+x = len(sys.argv) - 1
+if (x == 2):
+    print("Hello user, good job, you provided two arguments")
+elif (x == 0):
+    print("This script requires exactly two arguments. No arguments were provided!")
+else:
+    print("This script requires exactly two arguments. You provided", x, "arguments.")
