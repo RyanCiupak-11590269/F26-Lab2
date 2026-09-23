af0097c45a94bc840268389b418c4964032d1ffe
Ryan Ciupak <rciupak@myseneca.ca>
2026-09-23 17:58:13 +0000

    Completed

--- lab2k.py
+++ lab2k.py
@@ -1,17 +1,15 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Ryan Ciupak
+# Date: September 23, 2026
 # Purpose: use for loop.
 # Usage: ./lab2k.py
 
-# TO DO 1: 
-#Follow the instructions given in the README.md file.
-fruits = ["apple", "banana", "cherry", "date"]
 
-# Use a for loop to iterate over the list
-#for fruit in fruits:
-#    print(fruit)
-
-#for loop is commonly used with range functions. Here's another example using the range function to print numbers from 0  to 5.
+total = 0
+i = 0
+for i in range(101):
+    if i % 2 == 0:
+        total = total + i
+print(total)
